8259977f44d398834e8706e50436297a5dc98446
ual-laptop <ianfielding@arizona.edu>
2023-05-07 21:23:52 -0700

    made minor edits to server

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-app.use(express.static("tetrisHelpers"));
+const path = require('path')
+app.use('/static', express.static(path.join(__dirname, 'homepage')));
 app.use(express.json());
 
 app.listen(port, function() {
     console.log(`App listening at http://localhost:${port}`);
 });
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html");
+});
+
+[
+    "style.css",
+    "pixi.js",
+    "gui.js",
+    "tetris.js",
+    "agents.js"
+].forEach(function(com){
+    app.get(`/tetrisHelpers/${com}`,function(req,res){
+        res.sendFile(__dirname+`/tetrisHelpers/${com}`);
+    });
+})
